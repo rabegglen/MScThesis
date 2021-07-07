d2457b7290d55bbd5a8849e347c43dd049ca1f17
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-07 21:58:52 +0200

    added univariate normality test

--- SEM.R
+++ SEM.R
@@ -3,6 +3,12 @@
 source("constructs.R")
 
 
+pacman :: p_load(
+  lavaan,
+  mvnormalTest,
+  install = TRUE
+)
+
 ## introduce a variable which is coding for anthropomorphism and for empathy separately
 
 semDat = calcDat %>% 
@@ -22,3 +28,28 @@ semDat = calcDat %>%
       
     )
   )
+
+
+
+## shapiro-wilk univariate normality test
+
+univnorm = semDat %>% 
+  select(
+    technologyPlayfulness:UseContIntentionsAll
+  ) %>% 
+  mutate_all(., as.numeric) 
+
+
+
+
+for(i in 1 : ncol(univnorm)){
+  
+  test = mardia(univnorm[i])
+  names(univnorm[i]) %>% print()
+  test$uv.shapiro %>% print()
+  test$mv.test %>% print()
+  
+}
+
+
+
