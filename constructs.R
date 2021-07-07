dabc197f3548ab4ec9e554a5b2795b7d85b35aae
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-07 07:38:52 +0200

    adding script compiling constructs

---
+++ constructs.R
@@ -0,0 +1,61 @@
+#### Constructs
+
+source("dataPreparation.R")
+
+
+#### transforming all constructs to numeric
+
+## getting all the different levels of answers
+
+## general agreeance
+unique(calcDat$RobotSE1_1) %>% na.omit()
+
+## disconfirmation
+unique(calcDat$PI_TrDisc_Helpful_3) %>% na.omit()
+
+## satisfaction
+unique(calcDat$PI_Tech_Satisfact1) %>% na.omit()
+unique(calcDat$PI_Tech_Satisfact2) %>% na.omit()
+unique(calcDat$PI_Tech_Satisfact3) %>% na.omit()
+unique(calcDat$PI_Tech_Satisfact4) %>% na.omit()
+
+
+
+## recoding
+
+temp = calcDat %>% 
+  
+  select(-StartDate, -EndDate) %>% ## dates always cause trouble due to posixct standards interfering with other operations
+  
+  ## recode all text to numbers
+  
+  mutate_all(.,
+         funs(
+           case_when(
+             grepl("extremely disagree|^strongly disagree$|^much worse$|^extremely dissatisfied$|^Extremely miserable$|^extremely frustrated|extremely displeased", ., ignore.case = TRUE) ~ "1",
+             grepl("^disagree$|^worse$|^dissatisfied$|^miserable$|^frustrated$|^Displeased$", ., ignore.case = TRUE) ~ "2",
+             grepl("^somewhat disagree$|^somewhat worse$|^somewhat dissatisfied$|^somewhat miserable$|^somewhat frustrated$|^somewhat displeased$", ., ignore.case = TRUE) ~ "3",
+             
+             grepl("^neutral$", ., ignore.case = TRUE) ~ "4",
+             
+             grepl("^somewhat agree$|^somewhat better$|^somewhat satisfied$|^somewhat delighted$|^somewhat contented$|^somewhat Pleased$", ., ignore.case = TRUE) ~ "5",
+             grepl("^agree$|^better$|^satisfied$|^delighted$|^contented$|^Pleased$", ., ignore.case = TRUE) ~ "6",
+             grepl("^strongly agree$|^much better$|^extremely satisfied$|^extremely delighted$|^Extremely contented$|^Extremely Pleased$", ., ignore.case = TRUE) ~ "7", 
+             
+             TRUE ~ as.character(.) ### converting back afterwards to numeric
+           )
+           
+         )    
+             )
+
+
+
+#### preparing the constructs & recoding all reversed items
+
+revItems = names(calcDat) %>% 
+  .[grepl("rev$", .)]
+
+
+
+revDat = 
+
