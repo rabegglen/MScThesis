d840d77dba8f0bb4d3127b5ac53616b50c82f046
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-07 20:58:30 +0200

    renamed script

---
+++ descriptiveAnalysis.R
@@ -0,0 +1,41 @@
+### Descriptive analyses
+
+source("dataPreparation.R")
+
+load("./dataSet.RData")
+
+
+
+if(!require("pacman")){
+  
+  install.packages("pacman")
+  
+}
+
+# devtools :: install_github("alishinski/lavaanPlot")
+
+pacman :: p_load(
+  lavaan,
+  lavaanPlot,
+  devtools,
+  install = TRUE
+  
+)
+
+
+
+### some descriptive statistics
+
+desc = calcDat %>% 
+  count(Age_1)
+
+
+lang = calcDat %>% 
+  count(language)
+
+
+edu = calcDat %>% 
+  count(Edu) %>% 
+  mutate(
+    percentage = n / sum(n) * 100
+  )
