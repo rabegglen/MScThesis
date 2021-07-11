3274162c78ebb8a5df50fac376d8471dfca98da0
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-11 12:49:17 +0200

    adding heat map

--- descriptiveAnalysis.R
+++ descriptiveAnalysis.R
@@ -1,21 +1,23 @@
 ### Descriptive analyses and graphics
 
-# source("dataPreparation.R")
 source("plsPath4.R")
-
-# load("./dataSet.RData")
-
-
+dir.create("./tables")
+dir.create("./graphs")
 
 ### some simple descriptive statistics
+
+## Age
 
 desc = calcDat %>% 
   count(Age_1)
 
+## language
 
 lang = calcDat %>% 
   count(language)
 
+
+## education
 
 edu = calcDat %>% 
   count(Edu) %>% 
@@ -24,14 +26,57 @@ edu = calcDat %>%
   )
 
 
+## distribution within the different treatement levels
+
 groups = calcDat %>% 
   count(., group)
+
+
+
+
 
 ### get some heat maps
 
 
+## create matrix
 crossloadings = anthroModel4$crossloadings
 
+loadingNames = crossloadings$name
+
+
+hmCrossl = crossloadings %>% 
+  select(-block) %>% 
+  melt(., value.name = "Cross Loading") %>% 
+  rename(
+    Block = variable, Item = name 
+  ) %>% 
+  filter(
+    !grepl("innov|trustingstance|gender|edunum|playful|robotse", ignore.case = TRUE, Block) & !grepl("innov|trustingstance|gender|edunum|playful|robotse", ignore.case = TRUE, Item)
+  ) 
+
+
+
+## create raster plot
+
+
+ggplot(hmCrossl, aes(x = Block, y = Item, fill = `Cross Loading`)) + 
+  geom_raster() +
+  scale_x_discrete(guide = guide_axis(angle = 45)) +
+  labs(title = "Cross Loadings for Item Blocks") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_viridis(option = "magma") +
+  ggsave(
+    width = 30, height = 20, units = "cm", filename = "./graphs/CrossloadingsHeatMap.svg", dpi = 300
+  ) 
+
+
+### now the same as a Stargazer Table
+
+crossloadings %>% 
+  filter(
+    !grepl("innov|trustingstance|gender|edunum|playful|robotse", ignore.case = TRUE, name)
+  ) %>% 
+  stargazer(., type = "html", title = "Cross Loadings", summary = FALSE, out = "tables/crossLoadings.html", rownames = FALSE)
 
 
 
