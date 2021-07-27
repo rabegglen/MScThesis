9285ff9090bd5edd2f18a5ca08418a653c440729
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-27 16:10:19 +0200

    added randomisation check

--- scripts/manipulationCheck.R
+++ scripts/manipulationCheck.R
@@ -17,7 +17,6 @@ pacman :: p_load(
 
 
 MCdat = calcDat %>% 
-  select(matches("group|^MC_")) %>% 
   mutate(
     anthropo = case_when(
       
@@ -32,6 +31,47 @@ MCdat = calcDat %>%
       grepl("N$", group) ~ 0
       
     ) %>% as.factor(),
+    
+    eduNum = case_when(
+      
+      grepl("Mandatory School", Edu, ignore.case = TRUE) ~ 1,
+      grepl("Vocational Education and Training/Professional Certificate", Edu, ignore.case = TRUE) ~ 2,
+      grepl("Grammar school or equivalent", Edu, ignore.case = TRUE) ~ 3,
+      grepl("Bachelor's degree or equivalent", Edu, ignore.case = TRUE) ~ 4,
+      grepl("Master's degree", Edu, ignore.case = TRUE) ~ 5,
+      grepl("PhD, MD or higher", Edu, ignore.case = TRUE) ~ 6
+      
+    ),
+    
+    
+    # Creating meaningful numeric measures for control vars like age, gender and education
+    
+    genderNum = case_when(
+      
+      grepl("Female", Gender) ~ 2,
+      grepl("Male", Gender) ~ 1,
+      grepl("No Answer", Gender) ~ 0
+    ),
+    
+    
+    
+    ageNum = case_when(
+      
+      grepl("Younger than 20", Age_1, ignore.case = TRUE) ~ 1,
+      grepl("20-29", Age_1, ignore.case = TRUE) ~ 2,
+      grepl("30-39", Age_1, ignore.case = TRUE) ~ 3,
+      grepl("40-49", Age_1, ignore.case = TRUE) ~ 4,
+      grepl("50-59", Age_1, ignore.case = TRUE) ~ 5,
+      grepl("60-69", Age_1, ignore.case = TRUE) ~ 6,
+      grepl("70-79", Age_1, ignore.case = TRUE) ~ 7,
+      grepl("Older than 80", Age_1, ignore.case = TRUE) ~ 8,
+      
+    ),
+    
+    
+  ) %>% 
+  mutate(
+    group = as.factor(group)
   )
 
 
@@ -61,7 +101,8 @@ anthro = MCdat %>%
 
 # Kurskal Test
 kruskal.test(MCAnthro ~ anthropo, data=anthro) # significant
-anthromod = lm(MCAnthro ~ anthropo, data = anthro) 
+anthromod = lm(MCAnthro ~ anthropo, data = anthro) %>% 
+  summary()
 
 
 
@@ -93,7 +134,8 @@ kruskal.test(MCEmpath ~ empathy, data=empath) # significant
 
 
 # see what an LM can tell us? 
-empathmod = lm(MCEmpath ~ empathy, data = empath) 
+empathmod = lm(MCEmpath ~ empathy, data = empath) %>% 
+  summary()
 
  
 
@@ -111,4 +153,57 @@ stargazer(
 
 
 
- 
+
+
+
+
+
+
+
+##### checking the demographics
+
+
+
+lm(genderNum ~ group, data = MCdat) %>% 
+  summary()
+
+lm(eduNum ~ group, data = MCdat) %>% 
+  summary()
+
+lm(ageNum ~ group, data = MCdat) %>% 
+  summary()
+
+
+
+### control vars
+
+
+MCdat$technologyPlayfulness
+MCdat$techhnologyInnovativeness
+MCdat$generalTrustingStance
+MCdat$robotSelfEfficacy
+
+
+
+# innovativeness
+
+lm(techhnologyInnovativeness ~ group, data = MCdat) %>% 
+  summary()
+
+# Playfulness
+
+lm(technologyPlayfulness ~ group, data = MCdat) %>% 
+  summary()
+
+
+
+# Trusting Stance
+
+lm(generalTrustingStance ~ group, data = MCdat) %>% 
+  summary()
+
+
+# rotob self-efficacy
+
+lm(robotSelfEfficacy ~ group, data = MCdat) %>% 
+  summary()
