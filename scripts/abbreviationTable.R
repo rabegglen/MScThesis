b7886b45f226c4733cddf0f373f06c1656982043
Roman Abegglen <roman.abegglen@students.unibe.ch>
2021-07-28 21:19:24 +0200

    abbreviation table

---
+++ scripts/abbreviationTable.R
@@ -0,0 +1,19 @@
+### abbreviation table
+
+pacman :: p_load(
+  tidyverse,
+  stargazer,
+  install = TRUE
+)
+
+
+
+abbr = list.files("./Data/", full.names = TRUE) %>%
+  .[grepl("abbrevia.+\\.xlsx", ., ignore.case = TRUE)] %>% 
+  read_excel(.) 
+
+
+abbr %>% 
+  stargazer(
+    ., type = "html", summary = FALSE, out = "./tables/abbreviations.html", rownames = FALSE
+  )
